32791d09927aa1190b90faaa72e3a5cb6c562e10
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-03-10 23:52:31 +0200

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -56,6 +56,7 @@ class AuthController extends Controller
             'name' => 'required|string|between:2,100',
             'email' => 'required|string|email|max:100|unique:users',
             'password' => 'required|string|confirmed|min:6',
+            'is_admin' => 'optional|boolean'
         ]);
 
         if ($validator->fails()) {
