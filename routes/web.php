0ad62f8009e45532a28ab2e202ba8227fb872f18
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-03-10 20:27:14 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Routes::get("/login", function (){
+ return response()->json(["message"=>"you should
+  login or register first"],200);
+})->name("login");
