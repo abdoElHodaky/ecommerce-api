26193a907c08e6c721394a5d1a0928480df4c4d1
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-03-16 10:48:43 +0200

    Create 2020_12_12_091609_create_attachments_table.php

---
+++ database/migrations/2020_12_12_091609_create_attachments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttachmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attachments', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug')->unique();
+            $table->integer('user_id');
+            $table->integer('product_id');
+            $table->string("contentType");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attachments');
+    }
+}
