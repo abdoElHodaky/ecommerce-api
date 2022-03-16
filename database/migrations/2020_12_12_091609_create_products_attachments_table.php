9d22d2f1c2a9853f567c3baf2480bec31f5a78cd
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-03-16 15:35:35 +0200

    Update and rename 2020_12_12_091609_create_attachments_table.php to 2020_12_12_091609_create_products_attachments_table.php

--- database/migrations/2020_12_12_091609_create_products_attachments_table.php
+++ database/migrations/2020_12_12_091609_create_products_attachments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAttachmentsTable extends Migration
+class CreateProductsAttachmentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,13 @@ class CreateAttachmentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('attachments', function (Blueprint $table) {
+        Schema::create('products_attachments', function (Blueprint $table) {
             $table->id();
             $table->string('slug')->unique();
             $table->integer('user_id');
             $table->integer('product_id');
-            $table->string("contentType");
+            $table->string("content_type");
+            $table->string("path");
             $table->timestamps();
         });
     }
@@ -30,6 +31,6 @@ class CreateAttachmentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('attachments');
+        Schema::dropIfExists('products_attachments');
     }
 }
