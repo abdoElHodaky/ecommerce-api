1b913284c5507c65b86e9554ac1ea73a06662851
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-03-17 21:14:17 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/routes', function () {
-    return response()->file(file_get_contents("./routesl.txt"),[
-    "Content-Type"=>"text/plain",
-    200
-   ]);
+    return response()->download("./routesl.txt",[],"inline");
 });
 Route::get("/login", function (){
  return response()->json(["message"=>"you should
