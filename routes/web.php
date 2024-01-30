fdcc8993ac5309283e5d99edadf005c9187abe07
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-01-30 15:19:14 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+   // return view('welcome');
+    return file_get_contents(public_path()."/swagger.html");
 });
 Route::get('/routes', function () {
     return response()->download("./routesl.txt","routesl.txt",[],"inline");
